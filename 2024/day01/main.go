a4a4d833e25112101aa10876703db6a0e756b553
Maurice Kremer <maurice.kremer@gmail.com>
2024-12-01 10:21:07 +0100

    tweak

--- 2024/day01/main.go
+++ 2024/day01/main.go
@@ -13,14 +13,17 @@ func main() {
 	readfile, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+	defer readfile.Close()
+
 	scanner := bufio.NewScanner(readfile)
 	var numbers1 []int64
 	var numbers2 []int64
 
 	for scanner.Scan() {
-		strings.Fields(scanner.Text())
-		for index, numstr := range strings.Fields(scanner.Text()) {
+		lineParts := strings.Fields(scanner.Text())
+		for index, numstr := range lineParts {
 			number, _ := strconv.ParseInt(numstr, 10, 64)
 			if index == 0 {
 				numbers1 = append(numbers1, number)
@@ -36,12 +39,14 @@ func main() {
 	var diff int64
 	diff = 0
 	for i, _ := range numbers1 {
-		if numbers1[i] > numbers2[i] {
-			diff += numbers1[i] - numbers2[i]
-		} else {
-			diff += numbers2[i] - numbers1[i]
-		}
-
+		diff += abs(numbers1[i] - numbers2[i])
 	}
 	fmt.Println(diff)
+}
+
+func abs(x int64) int64 {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
